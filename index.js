c00a9a1b089b5730eb93d4132e704afe56f36950
+ <+>
2023-06-13 13:06:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+let random = 12
+let num3=24
+let num4=6
+let mod = num3/num4
+let max = Math.max(10,11,12,14,15,20)
 // Write your code here
